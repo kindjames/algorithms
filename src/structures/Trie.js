2697edacacf3251c1deb70c3a518fb1fc8f9c6d9
James Kind <hello@jameskind.com>
2019-12-21 14:05:09 -0800

    Simplify Trie implementation and return bool for addWord()

--- src/structures/Trie.js
+++ src/structures/Trie.js
@@ -1,54 +1,40 @@
 class Trie {
   rootNode = new Map()
 
-  add(item) {
-    // check is iterable item
-    if (Symbol.iterator in Object(item)) {
-      let currentNode = this.rootNode
+  addWord(word) {
+    let currentNode = this.rootNode
 
-      // for (const element of item) {
-      for (let i = 0; i < item.length; i++) {
-        const element = item[i]
-
-        if (currentNode.has(element)) {
-          currentNode = currentNode.get(element)
-        } else {
-          let temp = new Map()
-          currentNode.set(element, temp)
-          currentNode = temp
-        }
-
-        // did we reach the last element yet?
-        if (i === item.length - 1) {
-          // add signifier for end
-          currentNode.set('*', '*')
-        }
+    for (const letter of word) {
+      if (currentNode.has(letter)) {
+        currentNode = currentNode.get(letter)
+      } else {
+        let temp = new Map()
+        currentNode.set(letter, temp)
+        currentNode = temp
       }
     }
+
+    if (currentNode.has('*')) {
+      return false
+    }
+
+    // add signifier for end
+    currentNode.set('*', '*')
+    return true
   }
 
-  exists(item) {
-    // check is iterable item
-    if (Symbol.iterator in Object(item)) {
-      let currentNode = this.rootNode
+  wordExists(word) {
+    let currentNode = this.rootNode
 
-      for (let i = 0; i < item.length; i++) {
-        const element = item[i]
-        currentNode = currentNode.get(element)
+    for (const letter of word) {
+      currentNode = currentNode.get(letter)
 
-        if (currentNode === undefined) {
-          return false
-        }
-
-        // did we reach the last element yet?
-        if (i === item.length - 1) {
-          // add signifier for end
-          return currentNode.has('*')
-        }
+      if (currentNode === undefined) {
+        return false
       }
     }
 
-    return false
+    return currentNode.has('*')
   }
 }
 
--- test/structures/Trie.test.js
+++ test/structures/Trie.test.js
@@ -2,19 +2,22 @@ import test from 'ava'
 import { Trie } from '../../src/structures'
 
 test(`exists() returns true when an item was added`, t => {
-  let tree = new Trie()
-  tree.add('foo')
-  tree.add('bar')
-
-  t.true(tree.exists('foo'))
-  t.true(tree.exists('bar'))
+  let trie = new Trie()
+  t.is(trie.addWord('catch'), true, 'new word 1');
+  t.is(trie.addWord('cakes'), true, 'new word 2');
+  t.is(trie.addWord('cake'), true, 'prefix of existing word');
+  t.is(trie.addWord('cake'), false, 'word already present');
+  t.is(trie.addWord('caked'), true, 'new word 3');
+  t.is(trie.addWord('catch'), false, 'all words still present');
+  t.is(trie.addWord(''), true, 'empty word');
+  t.is(trie.addWord(''), false, 'empty word present');
 })
 
 test(`exists() returns false for a subset of them item`, t => {
   let tree = new Trie()
-  tree.add('foo')
-  tree.add('bar')
+  tree.addWord('foo')
+  tree.addWord('bar')
 
-  t.false(tree.exists('fo'))
-  t.false(tree.exists('barr'))
+  t.false(tree.wordExists('fo'))
+  t.false(tree.wordExists('barr'))
 })
